fbb43850651037ea2bc2b4a2480c306970d47010
Najim Uddin <najim7304@gmail.com>
2023-01-10 22:44:39 +0600

    toggle

--- app.vue
+++ app.vue
@@ -1,10 +1,17 @@
 <template>
     <!-- <Home-Welcome /> -->
-  <HomeWelcome />
+  <!-- <HomeWelcome /> -->
   <!-- <Counter /> -->
   <!-- Dynamic Components  -->
-  <component :is="Counter"/>
+  <!-- <component :is="Counter"/> -->
+  <button @click="toggle">Toggle</button>
+  <component :is="myComponent"/>
 </template>
 <script setup>
-const Counter = resolveComponent("Counter")
+// const Counter = resolveComponent("Counter")
+const myComponent = ref(resolveComponent("Counter"));
+function toggle (){
+  console.log(myComponent.value)
+  myComponent.value = resolveComponent("Home-Welcome")
+}
 </script>
