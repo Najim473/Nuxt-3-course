fd9eaa5cf33e0d510fd4c55fe1d70ce3b595ef40
Najim Uddin <najim7304@gmail.com>
2023-01-11 17:16:23 +0600

    by clicking add navigateTo in routes

--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,11 @@
     <h2>Hello index page</h2>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="about">About</NuxtLink>
+    <!-- By clicking redirect page  -->
+    <button @click="moveToAbout">Take me to about page</button>
 </template>
+<script setup>
+const moveToAbout =()=>{
+    navigateTo("about")
+}
+</script>
